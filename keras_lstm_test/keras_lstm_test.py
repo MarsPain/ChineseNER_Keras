e491c2e6fc2f6d5a2246207ef9a58c137ffd29f3
MarsPain <1434579002@qq.com>
2018-05-15 16:58:45 +0800

    生成训练集和验证集

--- keras_lstm_test/keras_lstm_test.py
+++ keras_lstm_test/keras_lstm_test.py
@@ -13,7 +13,7 @@ text_data_dir = data_dir + '/20_newsgroup/'
 max_sequence_length = 1000
 max_nb_words = 20000
 embedding_dim = 100
-validation_split = 0.2
+validation_split = 0.2  #验证集比例
 batch_size = 32
 
 #将预训练的词向量文件中的词向量转换为容易查询的字典
@@ -56,6 +56,26 @@ tokenizer = Tokenizer(nb_words=max_nb_words)
 tokenizer.fit_on_texts(texts)   #以texts作为训练的文本列表
 sequence = tokenizer.texts_to_sequences(texts)  #将文本列表转换为序列列表，每个序列对应一段文本
 word_index = tokenizer.word_index   #将字符串(word)映射为它们作为索引的排名（如ChineseNER中用出现次数作为排名）
-print(word_index['hi'])
-print(sequence[2][:20])
+# print(word_index['hi'])
+# print(sequence[2][:20])
 print("找到 %s 个单词" % len(word_index))
+
+#生成Train和validate数据集
+data = pad_sequences(sequence, maxlen=max_sequence_length)  #对序列进行填充处理
+labels = to_categorical(np.asarray(labels)) #将多类别label转换为one-hot向量
+# print("shape of data tensor:", data.shape)
+# print("shape of label tensor:", labels.shape)
+indices = np.arange(data.shape[0])
+# print(indices)
+np.random.shuffle(indices)  #打乱索引顺序
+data = data[indices]    #按照打乱的顺序调整data和label，因为原顺序是按照类别排列的，不好划分验证集和训练集
+labels = labels[indices]
+nb_validation_samples = int(validation_split*data.shape[0]) #验证集数量
+x_train = data[:-nb_validation_samples]
+y_train = labels[:-nb_validation_samples]
+# print(x_train.shape)
+x_val = data[-nb_validation_samples:]
+y_val = labels[-nb_validation_samples:]
+print("训练集和验证集已准备好")
+
+#生成词嵌入矩阵（embedding matrix）
