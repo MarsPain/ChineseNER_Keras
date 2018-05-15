0c2bcb6afe137b0aefed2f262d5fd32e264ef3b8
MarsPain <1434579002@qq.com>
2018-05-15 16:32:12 +0800

    对训练文本texts进行处理，并生成所有词的映射位置

--- keras_lstm_test/keras_lstm_test.py
+++ keras_lstm_test/keras_lstm_test.py
@@ -25,11 +25,37 @@ with open(os.path.join(data_dir, 'glove.6B.100d.txt'), encoding='utf-8') as f:
          #相应word的词向量vector，asarray与array都是将结构数据转化为array，区别在于asarray不会占用新内存
         vector = np.asarray(values[1:], dtype='float32')
         embedding_index[word] = vector
+# print(embedding_index['hi'])
 print("已匹配 %s 词向量" % len(embedding_index))
 
 print("对文本数据进行处理")
 texts = []  #text样本列表
-labels_index = {}   #字典
+labels_index = {}   #text原类名和label ID的字典
 labels = [] #标签label的列表
 for name in sorted(os.listdir(text_data_dir)):
-    print(name)
+    #遍历处理每种类别的text文件夹
+    path = os.path.join(text_data_dir, name)
+    if os.path.isdir(path):
+        #为每种类别赋值label ID
+        label_id = len(labels_index)    #一个小trick，根据
+        labels_index[name] = label_id
+        for file_name in sorted(os.listdir(path)):
+            #遍历处理每种类别下的所有text文件
+            if file_name.isdigit():
+                with open(os.path.join(path, file_name), encoding='latin-1') as file_path:
+                    # print(sys.version_info)
+                    # 将每个text文件及其对应的label添加到相应数组中
+                    texts.append(file_path.read())
+                    labels.append(label_id)
+# print(labels_index['alt.atheism'])
+# print(labels_index['comp.sys.ibm.pc.hardware'])
+print('找到 %s 个texts' % len(texts))
+
+#利用keras的Tokenizer对所有texts进行处理，主要将texts中的word映射到相应的index
+tokenizer = Tokenizer(nb_words=max_nb_words)
+tokenizer.fit_on_texts(texts)   #以texts作为训练的文本列表
+sequence = tokenizer.texts_to_sequences(texts)  #将文本列表转换为序列列表，每个序列对应一段文本
+word_index = tokenizer.word_index   #将字符串(word)映射为它们作为索引的排名（如ChineseNER中用出现次数作为排名）
+print(word_index['hi'])
+print(sequence[2][:20])
+print("找到 %s 个单词" % len(word_index))
