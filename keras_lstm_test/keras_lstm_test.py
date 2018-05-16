b59e980d4f53cf510ae434c780ad425ba01282ea
MarsPain <1434579002@qq.com>
2018-05-16 17:11:58 +0800

    测试写函数式模型和另一种保存模型的方法，遗留了函数式模型无法运行（tensor传输过程中shape不匹配的问题）

--- keras_lstm_test/keras_lstm_test.py
+++ keras_lstm_test/keras_lstm_test.py
@@ -5,9 +5,9 @@ import random
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils.np_utils import to_categorical
-from keras.models import Sequential, load_model
-from keras.layers import Embedding, LSTM, Dense, Activation
-from keras.callbacks import TensorBoard
+from keras.models import Sequential, load_model, Model
+from keras.layers import Embedding, LSTM, Dense, Activation, Input
+from keras.callbacks import TensorBoard, ModelCheckpoint
 
 data_dir = "data"
 text_data_dir = data_dir + '/20_newsgroup/'
@@ -97,30 +97,41 @@ print("embedding_matrix构建完成")
 
 #构建模型
 #序列式模型
-embedding_layer = Embedding(nb_words+1,
-                            embedding_dim,
-                            weights=[embedding_matrix],
+embedding_layer = Embedding(nb_words+1,embedding_dim,weights=[embedding_matrix],
                             input_length=max_sequence_length)
 model = Sequential()
 model.add(embedding_layer)
 model.add(LSTM(100, dropout=0.2))
 #该全连接层的神经元数量是不是太少了？，设置成32、然后activation设置为relu如何？
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(32, activation='relu'))
 model.add(Dense(len(labels_index), activation='softmax'))
 # model.layers[1].trainable=False #在model.summary中将以训练的参数和未训练的参数区分开？此处参数为词向量
 model.summary()
 
 #函数式模型
+# inputs = Input(shape=(100,))
+# # print(inputs.shape)
+# word_emb = Embedding(nb_words+1,embedding_dim,weights=[embedding_matrix],
+#               input_length=max_sequence_length)(inputs)
+# # print(word_emb.shape)
+# lstm = LSTM(100, dropout=0.2)(word_emb)
+# # print(lstm.shape)
+# dense = Dense(32, activation='relu')(lstm)
+# predictions = Dense(len(labels_index), activation='softmax')(dense)
+# model = Model(inputs=inputs, outputs=predictions)
+# model.summary()
 
 #训练模型
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
+check_pointer = ModelCheckpoint(filepath='best_model.h5', verbose=1,  save_best_only=True)
 print("开始训练。。。")
-model.fit(x_train, y_train, batch_size=batch_size, epochs=1)
-model.save('model_1.h5')
-del model
-model.load_model('model_1.h5')
+model.fit(x_train, y_train, batch_size=batch_size, epochs=3,
+          validation_data=(x_val, y_val),callbacks = [check_pointer])
+# model.save('model_2.h5')
+# del model
+# model.load_model('model_2.h5')
 score, acc = model.evaluate(x_val, y_val, batch_size=batch_size)
 print('Test score:', score)
 print('Test accuracy:', acc)
