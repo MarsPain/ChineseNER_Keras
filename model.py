b70762edc0ec86c52edf37b241aa5237080caff9
MarsPain <1434579002@qq.com>
2018-05-31 11:19:23 +0800

    用keras搭建的CRF层替换keras-contrib包引入的CRF层

--- model.py
+++ model.py
@@ -6,8 +6,9 @@ from keras.models import Sequential, load_model, Model,optimizers
 from keras.layers import Embedding, LSTM, Dense, Activation, Input, Bidirectional, \
     TimeDistributed, Dropout, concatenate
 from keras.callbacks import TensorBoard, ModelCheckpoint
-from keras_contrib.layers import CRF
+# from keras_contrib.layers import CRF
 from keras.initializers import Zeros
+from keras_crf_test import CRF
 
 is_train = True
 is_save = True
@@ -37,7 +38,7 @@ def create_model(embedding_matrix, tag_index):
     dense = TimeDistributed(Dense(len(tag_index)))(bilstm)
     # print("dense:", dense)
     # model = Model(inputs=input, outputs=dense)
-    crf_layer = CRF(len(tag_index), sparse_target=True) #若后接CRF
+    crf_layer = CRF(len(tag_index)) #若后接CRF
     crf = crf_layer(dense)
     if seg_dim:
         model = Model(inputs=[char_input, seg_input], outputs=crf)
@@ -50,7 +51,7 @@ def create_model(embedding_matrix, tag_index):
     #               optimizer=optmr,
     #               metrics=['accuracy'])
     #若使用crf作为最后一层，则修改模型编译的配置：
-    model.compile(loss=crf_layer.loss_function, #注意这里的参数配置，crf_layer为对CRF()进行初始化的命名
+    model.compile(loss=crf_layer.loss, #注意这里的参数配置，crf_layer为对CRF()进行初始化的命名
                   optimizer=optmr,
                   metrics=[crf_layer.accuracy])
 
